3d3a006f61f86e3284ad1f9ac5700c4745db4149
zamir <baytzam@gmail.com>
2021-09-16 16:35:10 +0300

    Start

---
+++ dist/example/ExamplePage.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = ExamplePage;
+
+var _jsxRuntime = require("react/jsx-runtime");
+
+function ExamplePage() {
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("main", {
+    children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+      className: "container-fluid",
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("h1", {
+        children: "Example Page"
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("p", {
+        children: "Hello world From MFE!"
+      })]
+    })
+  });
+}
+//# sourceMappingURL=ExamplePage.js.map
