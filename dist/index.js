61cb995fd83d7182fdafef0b0ef95dc1443e1118
zamir <baytzam@gmail.com>
2021-09-17 14:13:01 +0300

    last

--- dist/index.js
+++ dist/index.js
@@ -1,46 +1,22 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-require("core-js/stable");
-
-require("regenerator-runtime/runtime");
-
-var _frontendPlatform = require("@edx/frontend-platform");
-
-var _react = require("@edx/frontend-platform/react");
-
-var _reactDom = _interopRequireDefault(require("react-dom"));
-
-var _frontendComponentHeader = _interopRequireWildcard(require("@edx/frontend-component-header"));
-
-var _frontendComponentFooter = _interopRequireWildcard(require("@edx/frontend-component-footer"));
-
-var _i18n = _interopRequireDefault(require("./i18n"));
-
-var _ExamplePage = _interopRequireDefault(require("./example/ExamplePage"));
-
-require("./index.scss");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = ExamplePage;
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
-
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-(0, _frontendPlatform.subscribe)(_frontendPlatform.APP_READY, function () {
-  _reactDom.default.render( /*#__PURE__*/(0, _jsxRuntime.jsxs)(_react.AppProvider, {
-    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_frontendComponentHeader.default, {}), /*#__PURE__*/(0, _jsxRuntime.jsx)(_ExamplePage.default, {}), /*#__PURE__*/(0, _jsxRuntime.jsx)(_frontendComponentFooter.default, {})]
-  }), document.getElementById('root'));
-});
-(0, _frontendPlatform.subscribe)(_frontendPlatform.APP_INIT_ERROR, function (error) {
-  _reactDom.default.render( /*#__PURE__*/(0, _jsxRuntime.jsx)(_react.ErrorPage, {
-    message: error.message
-  }), document.getElementById('root'));
-});
-(0, _frontendPlatform.initialize)({
-  messages: [_i18n.default, _frontendComponentHeader.messages, _frontendComponentFooter.messages]
-});
+function ExamplePage() {
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("main", {
+    children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+      className: "container-fluid",
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("h1", {
+        children: "Example Page"
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("p", {
+        children: "Hello world From MFE!"
+      })]
+    })
+  });
+}
 //# sourceMappingURL=index.js.map
--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,10 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
-import {
-  APP_INIT_ERROR, APP_READY, subscribe, initialize,
-} from '@edx/frontend-platform';
-import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
-import ReactDOM from 'react-dom';
-
-import Header, { messages as headerMessages } from '@edx/frontend-component-header';
-import Footer, { messages as footerMessages } from '@edx/frontend-component-footer';
-
-import appMessages from './i18n';
-import ExamplePage from './example/ExamplePage';
-
-import './index.scss';
-
-subscribe(APP_READY, () => {
-  ReactDOM.render(
-    <AppProvider>
-      <Header />
-      <ExamplePage />
-      <Footer />
-    </AppProvider>,
-    document.getElementById('root'),
+export default function ExamplePage() {
+  return (
+    <main>
+      <div className="container-fluid">
+        <h1>Example Page</h1>
+        <p>Hello world From MFE!</p>
+      </div>
+    </main>
   );
-});
-
-subscribe(APP_INIT_ERROR, (error) => {
-  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
-});
-
-initialize({
-  messages: [
-    appMessages,
-    headerMessages,
-    footerMessages,
-  ],
-});
+}
